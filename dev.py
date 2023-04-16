742a19ab30bcb4794cef8653ad97a7cdacbaabfb
Helen G Scott <helengracescott@gmail.com>
2023-04-16 10:43:08 -0400

    Expand maintenance to include water, Pi, and protons

--- dev.py
+++ dev.py
@@ -1,11 +1,23 @@
 # A function that checks if a specific reaction is a maintenace reaction
-# by checking if the reaction has one reactant and one product, and
-# those are ATP and ADP, respectively.
+# by checking if the reaction has only ATP and water as reactants and
+# only ADP, hydrogen, and phosphate as products.
 def is_maintenance_reaction(model, reaction):
-    if len(reaction.reactants) == 1 and len(reaction.products) == 1:
-        if reaction.reactants[0].id == 'cpd00002' and reaction.products[0].id == 'cpd00008':
-            return True
-    return False
+    # A list of the IDs of the reactants and products that are allowed
+    # in a maintenance reaction. Have to specify the compartment of the
+    # metabolites, this may be a problem if different models use
+    # different compartment IDs.
+    # ATP and water are allowed reactants.
+    allowed_reactants = ['cpd00002_c0', 'cpd00001_c0']
+    # ADP, phosphate, and protons are allowed products.
+    allowed_products = ['cpd00008_c0', 'cpd00009_c0', 'cpd00067_c0']
+    # Check if the reaction has only the allowed reactants and products
+    # in any order.
+    if any (met.id not in allowed_reactants for met in reaction.reactants):
+        return False
+    if any (met.id not in allowed_products for met in reaction.products):
+        return False
+    else:
+        return True
 
 # A function that searches all the reactions in a model for a maintenance
 # reaction, and returns the reaction if it finds one.
